6d318651c654d15049db7547d18848d9996144b0
Bazyli Kot <ciobab97@gmail.com>
2021-11-19 12:36:20 +0100

    working on scraper class

--- bet_crawler/main.py
+++ bet_crawler/main.py
@@ -2,8 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import sts
+import scraper
+import sys
 
 BASE_URL = "https://sts.pl"
+s = scraper.Scraper(BASE_URL)
+print(s.get_entries())
+sys.exit(0)
+
 
 request = requests.get(BASE_URL)
 request.raise_for_status()
@@ -11,7 +17,6 @@ request.raise_for_status()
 soup = BeautifulSoup(request.text, "html.parser")
 
 sports_table = soup.find(id="bets").find("ul")
-# print(sports_table)
 
 sport_entries = sports_table.find_all("li")
 
@@ -45,21 +50,3 @@ for entry in sport_entries:
         match = matches[0]
         bet = sts.Bet.from_col(match, discipline=discipline)
         print(bet)
-        # event = match.find("span", {"class": "liga"})
-        # match_name = match.find("td", {"class": "matchName"})
-        # start_time = match.find("td", {"class": "date_time"})
-        # start_day = match.find("span", {"class": "date"})
-        # bet = match.find_all("td", {"class": "bet"})
-        
-        # print('event:', re.sub("\s", "", ''.join(event.stripped_strings)))
-        # print('day:', re.sub("[a-zA-Z]|-|\s", "", ''.join(start_day.stripped_strings)))
-        # print('time:', ''.join(start_time.stripped_strings))
-        # if match_name:
-        #     print('name:', ''.join(match_name.stripped_strings))
-
-        # bet_stake = list(map(lambda x: tuple(x.stripped_strings), bet))
-        # print(bet_stake)
-
-    # print(discipline, number)
-
-    # print("=" * 10)
--- bet_crawler/scraper.py
+++ bet_crawler/scraper.py
@@ -1,6 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+
+
 class Scraper:
-    def __init__(self, base_url) -> None:
+    def __init__(self, base_url):
         self.base_url = base_url
+        self.entries = []
+
+    def get_entries(self):
+        out = []
+        request = requests.get(self.base_url)
+        request.raise_for_status()
+        soup = BeautifulSoup(request.text, "html.parser")
+        sports_table = soup.find(id="bets").find("ul")
+        sport_entries = sports_table.find_all("li")
+        for entry in sport_entries:
+            a = entry.find("a", href=True)
+            url = a["href"]
+            url_parts = url.split("/")
+
+            if len(url_parts) < 2:
+                # TODO: This could add some log about failure
+                continue
+
+            discipline = url_parts[-3]
+            out.append((discipline, self.base_url + url, ))
+
+        self.entries = out
+        return out
+
 
     def get_discipline_names(self):
         pass
