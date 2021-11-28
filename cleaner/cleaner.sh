9f32d7cb04b13163d1d3d79ae1f2bf1ff5f32057
Bazyli Kot <ciobab97@gmail.com>
2021-11-28 11:48:36 +0100

    main functionality on my way

--- cleaner/cleaner.sh
+++ cleaner/cleaner.sh
@@ -6,16 +6,14 @@ function parse_args {
 }
 
 
-# Associative array 
-# mapping formats to destination folder
-# eg ".pdf,".txt": "Documents" or "/home/bazyli/Documents"
 source utils.sh
 
+
 base_path="/home/bazyli/Programming/studia/PJS/smol_scripts/cleaner/test"
-source_dir=Downloads
+source_dir="test/Downloads"
 
 declare -A files_association
-files_association=( ["pdf,txt"]="Documents" \
+files_association=( ["pdf,txt"]="Documents"
                     ["png,jpg"]="Pictures"
                     ["mp3,wav"]="Music"
                     ["foo"]="wrong"
@@ -23,8 +21,8 @@ files_association=( ["pdf,txt"]="Documents" \
 
 print_info "Starting..."
 
-# figure out source dir 
-[ ! -d "$source_dir" ] && print_warning "Directory {./${source_dir}} do not exist." && \
+# figure out source dir
+[ ! -d "$source_dir" ] && print_warning "Directory {./${source_dir}} do not exist. Will try absolute path." && \
                           source_dir="${base_path}/${source_dir}"
 [ ! -d "$source_dir" ] && print_error "Directory {${source_dir}} do not exist!" &&
                           print_error "Cannot clean empty directory! Exiting..." && exit 1
@@ -34,22 +32,19 @@ print_info "Will clean $source_dir."
 # loop moving files into correcd directories
 for ext in ${!files_association[*]}
 do
-    echo
-    echo ext: $ext val: ${files_association[$ext]}
-
-    # figure out destination dir 
+    # figure out destination dir
     destination_dir=${files_association[$ext]}
     [ ! -d "$destination_dir" ] && destination_dir="${base_path}/${destination_dir}"
     [ ! -d "$destination_dir" ] && print_warning "Directory {${destination_dir}} do not exist!" \
     && continue
-    
+
     IFS=',' read -ra extensions <<< "$ext"
     for extension in "${extensions[@]}"
     do
-        echo ext: $extension dest: $destination_dir
         for file in "$source_dir"/*
         do
-            [[ $file =~ ^.*\.$extension$ ]] && echo "found $file"
+            [[ $file =~ ^.*\.$extension$ ]] && \
+            move_file $file $destination_dir
         done
     done
 
--- cleaner/utils.sh
+++ cleaner/utils.sh
@@ -3,28 +3,35 @@ QUIET=false
 DRY_RUN=true
 
 # Recreate log file every run
-:> $LOG_FILE
+$QUIET && :> $LOG_FILE
 
 function print_error {
+    $QUIET && return
     echo -e "ERROR: $*" | tee -a $LOG_FILE >&2
 }
 
 function print_warning {
+    $QUIET && return
     echo -e "WARNING: $*" | tee -a $LOG_FILE >&2
 }
 
 function print_info {
+    $QUIET && return
     echo -e "INFO: $*" | tee -a $LOG_FILE
 }
 
 function move_file {
+    $DRY_RUN && \
+    print_info "Would move {$1} to {$2}." && \
+    return
+
     mv "$1" "$2"
 }
 
 
 function clean_directory {
     base_path="$1"
-    to_clean="$2"
+    source_dir="$2"
     files_association="$3"
 }
 
